1b9ac936dd731605894adb29a82b5fee9a225284
Xavier Laffargue <laffargue.xavier@gmail.com>
2023-01-17 21:35:30 -0500

    Update MediaType.cs

--- SimpleM3u8Parser/DTO/MediaType.cs
+++ SimpleM3u8Parser/DTO/MediaType.cs
@@ -1,6 +1,21 @@
 namespace SimpleM3u8Parser;
 
-public enum MediaType
+public class MediaType
 {
-    NONE,AUDIO,VIDEO,CLOSEDCAPTIONS
+    private MediaType(string value)
+    {
+        _value = value;
+    }
+
+    private string _value { get; }
+
+    public static MediaType Audio => new MediaType("AUDIO");
+    public static MediaType Video => new MediaType("VIDEO");
+    public static MediaType Subtitles => new MediaType("SUBTITLES");
+    public static MediaType CloseCaptions => new MediaType("CLOSED-CAPTIONS");
+
+    public override string ToString()
+    {
+        return _value;
+    }
 }
