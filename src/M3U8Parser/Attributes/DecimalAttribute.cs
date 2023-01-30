9e36f754f99006f7d018bd46dcc877697a48ab88
Xavier Laffargue <laffargue.xavier@gmail.com>
2023-01-30 17:49:31 -0500

    fix

---
+++ src/M3U8Parser/Attributes/DecimalAttribute.cs
@@ -0,0 +1,35 @@
+using System.Globalization;
+
+namespace M3U8Parser.Attributes
+{
+    using System.Text.RegularExpressions;
+
+    public class DecimalAttribute : CustomAttribute<decimal>
+	{
+		public DecimalAttribute(string attributeName) : base(attributeName)
+		{
+		}
+
+		public override string ToString()
+		{
+			if (Value != null)
+			{
+				return $"{AttributeName}={Value.ToString(CultureInfo.InvariantCulture)}";
+			}
+
+			return string.Empty;
+		}
+		
+		public override void Read(string content)
+        {
+            var pattern = $"(?={AttributeName})(.*?)(?=,|$)";
+			var match = Regex.Match(content.Trim(), pattern, RegexOptions.Multiline & RegexOptions.IgnoreCase);
+			
+            if (match.Success)
+			{
+				var valueFounded = match.Groups[0].Value.Split('=')[1];
+				Value = decimal.Parse(valueFounded, CultureInfo.InvariantCulture);
+			}
+		}
+	}
+}
