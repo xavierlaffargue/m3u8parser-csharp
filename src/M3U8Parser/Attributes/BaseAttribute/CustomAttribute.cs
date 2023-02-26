521c0f6e4d5f06b05716404447b797ba60ce9b33
Xavier Laffargue <laffargue.xavier@gmail.com>
2023-02-26 11:54:58 -0500

    Update CustomAttribute.cs

--- src/M3U8Parser/Attributes/BaseAttribute/CustomAttribute.cs
+++ src/M3U8Parser/Attributes/BaseAttribute/CustomAttribute.cs
@@ -1,20 +1,20 @@
 namespace M3U8Parser.Attributes
 {
-    using M3U8Parser.CustomType;
+	using M3U8Parser.CustomType;
 	using M3U8Parser.Interfaces;
 	using System;
-    using System.Text.RegularExpressions;
+	using System.Text.RegularExpressions;
 
-    public class CustomAttribute<T> : IAttribute
+	public class CustomAttribute<T> : IAttribute
 	{
 		public CustomAttribute(string attributeName)
 		{
 			AttributeName = attributeName;
 		}
-        
+
 		public T Value { get; set; }
-        
-        protected string AttributeName { get; }
+
+		protected string AttributeName { get; }
 
 		public override string ToString()
 		{
@@ -28,20 +28,20 @@ namespace M3U8Parser.Attributes
 
 		public virtual void Read(string content)
 		{
-            var match = Regex.Match(content.Trim(), $"[,|:](?={AttributeName})(.*?)(?=,|$)",
+			var match = Regex.Match(content.Trim(), $"[,|:](?={AttributeName})(.*?)(?=,|$)",
 				RegexOptions.Multiline & RegexOptions.IgnoreCase);
 
-            var type = typeof(T);
-            
+			var type = typeof(T);
+
 			if (match.Success)
 			{
 				var valueFounded = match.Groups[0].Value.Split('=')[1];
 
-				if (type.IsGenericType && type.GetGenericTypeDefinition().Equals(typeof(Nullable<>))) 
+				if (type.IsGenericType && type.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
 				{
 					type = Nullable.GetUnderlyingType(type);
 				}
-				
+
 				if (typeof(ICustomAttribute).IsAssignableFrom(type))
 				{
 					var instanceAttribute = (ICustomAttribute)Activator.CreateInstance(type, false);
@@ -58,58 +58,4 @@ namespace M3U8Parser.Attributes
 			}
 		}
 	}
-    
-      public class CustomAttributeMediaPlaylist<T> : IAttribute
-    	{
-    		public CustomAttributeMediaPlaylist(string attributeName)
-    		{
-    			AttributeName = attributeName;
-    		}
-            
-    		public T Value { get; set; }
-            
-            protected string AttributeName { get; }
-    
-    		public override string ToString()
-    		{
-    			if (Value != null)
-    			{
-    				return $"{AttributeName}:{Value}";
-    			}
-    
-    			return string.Empty;
-    		}
-    
-    		public virtual void Read(string content)
-    		{
-                var match = Regex.Match(content.Trim(), $"(?<={AttributeName}:)(.*?)(?=$)",
-    				RegexOptions.Multiline & RegexOptions.IgnoreCase);
-    
-                var type = typeof(T);
-                
-    			if (match.Success)
-    			{
-    				var valueFounded = match.Groups[0].Value.Split('=')[1];
-    
-    				if (type.IsGenericType && type.GetGenericTypeDefinition().Equals(typeof(Nullable<>))) 
-    				{
-    					type = Nullable.GetUnderlyingType(type);
-    				}
-    				
-    				if (typeof(ICustomAttribute).IsAssignableFrom(type))
-    				{
-    					var instanceAttribute = (ICustomAttribute)Activator.CreateInstance(type, false);
-    					Value = (T)instanceAttribute.ParseFromString(valueFounded);
-    				}
-    				else
-    				{
-    					Value = (T)Convert.ChangeType(valueFounded, type);
-    				}
-    			}
-    			else
-    			{
-    				Value = default(T);
-    			}
-    		}
-    	}
 }
